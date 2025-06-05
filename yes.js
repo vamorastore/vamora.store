b33bc0b649f3a5eadad6e8c11020d52874044058
vamorastore <vamora.co.in@gmail.com>
2025-06-05 12:01:37 +0530

    Update yes.js

--- yes.js
+++ yes.js
@@ -1,12 +1,12 @@
-// Initialize Firebase
+// Initialize Firebase using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBkMUmD27GU34yIPQAj7KUErt9muB0MdLk",
-  authDomain: "vamora.store",
-  projectId: "vamora-co-in",
-  storageBucket: "vamora-co-in.appspot.com",
-  messagingSenderId: "613048727757",
-  appId: "1:613048727757:web:d0c73e84fa7d93a5c21184",
-  measurementId: "G-8R6TDRQGS6"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
