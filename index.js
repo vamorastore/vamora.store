a3a3e3586a9ebc5442142d520a94795f8001746a
vamorastore <vamora.co.in@gmail.com>
2025-06-05 14:51:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Razorpay = require('razorpay');
 const { initializeApp } = require("firebase/app");
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY,
+  key: process.env.RAZORPAY_KEY,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
