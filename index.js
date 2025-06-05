6d2ed37252b2ec12a816f1222a7286f0b248dc0b
vamorastore <vamora.co.in@gmail.com>
2025-06-05 14:37:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const Razorpay = require('razorpay');
+const { initializeApp } = require("firebase/app");
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
